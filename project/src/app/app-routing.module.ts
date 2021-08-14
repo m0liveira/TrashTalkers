252541244cef209f1f4a9319fe7f230f40c14374
Mateus Oliveira <75847514+m0liveira@users.noreply.github.com>
2021-08-14 20:02:36 +0100

    404 page done

--- project/src/app/app-routing.module.ts
+++ project/src/app/app-routing.module.ts
@@ -1,8 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FourOfourComponent } from './components/four-ofour/four-ofour.component';
 
 const routes: Routes = [
-  
+  {
+    path: '',
+    redirectTo: '/FourOFour',
+    pathMatch: 'full',
+  },
+  {
+    path: 'FourOFour',
+    component: FourOfourComponent,
+  },
+  {
+    path: '**',
+    redirectTo: '/FourOFour',
+    pathMatch: 'full',
+  },
 ];
 
 @NgModule({
--- project/src/app/app.module.ts
+++ project/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FourOfourComponent } from './components/four-ofour/four-ofour.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FourOfourComponent
   ],
   imports: [
     BrowserModule,
